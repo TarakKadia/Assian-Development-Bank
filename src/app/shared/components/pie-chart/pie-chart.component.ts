2a2571397ec7c9afe42910055022ccf0b1fa2eab
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 <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>
2022-01-05 17:02:38 +0530

    -Bug Solved

--- src/app/shared/components/pie-chart/pie-chart.component.ts
+++ src/app/shared/components/pie-chart/pie-chart.component.ts
@@ -22,34 +22,34 @@ export class PieChartComponent implements OnInit, AfterViewInit {
 
 
         
-        Chart.pluginService.register({
-            beforeDraw: function (chart: any) {
-                var width = chart.canvas.width,
-                    height = chart.canvas.height,
-                    ctx: any = chart.ctx
-                    weight: 10
+        // Chart.pluginService.register({
+        //     beforeDraw: function (chart: any) {
+        //         var width = chart.canvas.width,
+        //             height = chart.canvas.height,
+        //             ctx: any = chart.ctx
+        //             weight: 10
 
-                ctx.restore();
-                var fontSize = (height / 350).toFixed(2);
-                ctx.font = fontSize + "em sans-serif";
-                ctx.textBaseline = "middle";
-                var text = 'Total Commitments'
-                var text1 = 'By Region',
+        //         ctx.restore();
+        //         var fontSize = (height / 350).toFixed(2);
+        //         ctx.font = fontSize + "em sans-serif";
+        //         ctx.textBaseline = "middle";
+        //         var text = 'Total Commitments'
+        //         var text1 = 'By Region',
 
-                    textX = Math.round((width - ctx.measureText(text).width) / 2),
-                    textY = height / 2;
+        //             textX = Math.round((width - ctx.measureText(text).width) / 2),
+        //             textY = height / 2;
 
-                    textX = Math.round((width - ctx.measureText(text1).width) / 2),
-                    textY = height / 2;
+        //             textX = Math.round((width - ctx.measureText(text1).width) / 2),
+        //             textY = height / 2;
 
 
-                ctx.fillText(text, textX-20, textY+20);
-                ctx.fillText(text1, textX, textY+30);
-                ctx.save();
+        //         ctx.fillText(text, textX-20, textY+20);
+        //         ctx.fillText(text1, textX, textY+30);
+        //         ctx.save();
 
-            }
+        //     }
 
-        });
+        // });
     }
     
 
